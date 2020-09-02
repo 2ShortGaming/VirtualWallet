f1aacd12412e86c78df4fc822ac77f775af42585
wadecran <47024432+wadecran@users.noreply.github.com>
2020-09-02 11:44:53 -0400

    Initial commit

---
+++ VirtualWallet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VirtualWallet.Startup))]
+namespace VirtualWallet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
